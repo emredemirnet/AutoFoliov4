e72b3ab26301cd86f0081d4f922c010655a6d5b9
emredemirnet <97382572+emredemirnet@users.noreply.github.com>
2026-02-04 04:50:20 +0300

    Update and rename main.jsx to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AutoFolio from './AutoFolio.jsx'
+import AutoFolio from './AutoFolio.js'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
