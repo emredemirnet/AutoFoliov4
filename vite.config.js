d95c3e7dbd197da608774ebf3222e2159bd8bcf6
emredemirnet <97382572+emredemirnet@users.noreply.github.com>
2026-02-06 09:29:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,35 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+      crypto: 'crypto-browserify',
+    }
+  },
+  define: {
+    'process.env': {},
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    include: [
+      '@solana/wallet-adapter-base',
+      '@solana/wallet-adapter-react',
+      '@solana/wallet-adapter-react-ui',
+      '@solana/wallet-adapter-wallets',
+      '@solana/web3.js'
+    ],
+    esbuildOptions: {
+      target: 'esnext',
+      supported: {
+        bigint: true
+      }
+    }
+  },
+  build: {
+    target: 'esnext',
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
 })
