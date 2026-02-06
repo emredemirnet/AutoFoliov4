9c7d90d1c0f4e6e04863f36c1bc65e7239bb8fbb
emredemirnet <97382572+emredemirnet@users.noreply.github.com>
2026-02-06 04:51:05 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  },
-  optimizeDeps: {
-    include: ['@solana/web3.js']
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 })
